287ef46560588fe0f711dd51505a62c6bcb2eca7
Onat Ege <ege.karaman@bilgiedu.net>
2022-09-03 22:35:19 +0300

    Initial commit

---
+++ Data/CRUDApiDemoContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CRUDApiDemo.Models;
+
+namespace CRUDApiDemo.Data
+{
+    public class CRUDApiDemoContext : DbContext
+    {
+        public CRUDApiDemoContext (DbContextOptions<CRUDApiDemoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CRUDApiDemo.Models.Employee> Employee { get; set; } = default!;
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace CRUDApiDemo.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Wage { get; set; }
+
+    }
+}
